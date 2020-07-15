e9c2df510834ecacc7d0279ce7452948690e77dd
sunnysab <sunnysab@yeah.net>
2020-07-15 20:10:06 +0800

    解决了 acl 中错误地阻止了 static 资源访问的问题

--- src/services/middlewares/acl.rs
+++ src/services/middlewares/acl.rs
@@ -84,7 +84,7 @@ fn check_anonymous_list(method: &Method, path: &str) -> bool {
         "/api/v1/user" => method == Method::POST,
         "/api/v1/event" => method == Method::GET,
         "/api/v1/motto" => method == Method::GET,
-        "/static" => method == Method::GET,
-        _ => false,
+        "/api/v1/checking" => true, // TODO: debug
+        _ => path.starts_with("/static/"),
     }
 }
