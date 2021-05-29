b610285a520868ac21af9568e7786c8f48e72b08
B635 <xck15221368562@gmail.com>
2021-05-29 22:57:38 +0800

    更新搜索模块，添加页面搜索接口

--- src/models/search.rs
+++ src/models/search.rs
@@ -39,3 +39,58 @@ pub async fn query_notice(pool: &PgPool, query: &str, page: &PageView) -> Result
 
     Ok(result)
 }
+
+///pages
+///
+/// The pages in OA portal.
+#[derive(sqlx::FromRow, Serialize)]
+pub struct Page {
+    pub title: Option<String>,
+    pub host: Option<String>,
+    pub path: Option<String>,
+    pub publish_date: Option<NaiveDate>,
+    pub update_date: Option<NaiveDate>,
+    pub link_count: Option<i16>,
+    pub content: Option<String>,
+}
+
+pub async fn query_page(pool: &PgPool, query: &str, page: &PageView) -> Result<Vec<Page>> {
+    let result = sqlx::query_as(
+        "SELECT title, host, path, publish_date, update_date, link_count, content
+            FROM search.search_page($1)
+            ORDER BY publish_date DESC
+            OFFSET $2 LIMIT $3;",
+    )
+    .bind(query)
+    .bind(page.offset(20) as i32)
+    .bind(page.count(20) as i32)
+    .fetch_all(pool)
+    .await?;
+
+    Ok(result)
+}
+
+// ///attachments
+// ///
+// /// The pages in OA portal.
+// #[derive(sqlx::FromRow, Serialize())]
+// pub struct Attachment {
+//     pub id: i32,
+//     pub title: Option<String>,
+//     pub host: Option<String>,
+//     pub path: Option<String>,
+//     pub size: Option<i32>,
+//     pub local_name: Option<String>,
+//     pub checksum: Option<String>,
+//     pub referer: Option<String>,
+// }
+//
+// pub async fn query_attachment(pool: &PgPool, query: &str, page: &PageView) -> Result<Vec<Attachment>> {
+//     let result = sqlx::query_as(
+//         "",
+//     )
+//         .fetch_all(pool)
+//         .await?;
+//
+//     Ok(result)
+// }
--- src/services/handlers/search.rs
+++ src/services/handlers/search.rs
@@ -1,8 +1,9 @@
 use crate::error::{ApiError, Result};
-use crate::models::search::query_notice;
+use crate::models::search::{query_notice, SearchError, query_page};
 use crate::models::{CommonError, PageView};
 use crate::services::{response::ApiResponse, AppState, JwtToken};
 use actix_web::{get, web, HttpResponse};
+use crate::models::user::Person;
 
 #[derive(serde::Deserialize)]
 pub struct SearchReq {
@@ -20,14 +21,29 @@ pub async fn search(
     let sort = path.into_inner();
     let query = query.into_inner().query;
 
+    //User need log in before search
+    if token.is_none() {
+        return Err(ApiError::new(CommonError::LoginNeeded));
+    }
+    //User need identity before search
+    let uid = token.unwrap().uid;
+    if Person::get_identity(&app.pool, uid).await?.is_none() {
+        return Err(ApiError::new(SearchError::NeedIdentity));
+    }
+
     return match sort.as_str() {
         "notice" => {
-            if token.is_none() {
-                return Err(ApiError::new(CommonError::LoginNeeded));
-            }
             let result = query_notice(&app.pool, &query, &page).await?;
             Ok(HttpResponse::Ok().json(ApiResponse::normal(result)))
         }
+        "page" => {
+            let result = query_page(&app.pool, &query, &page).await?;
+            Ok(HttpResponse::Ok().json(ApiResponse::normal(result)))
+        }
+        // "attachment" => {
+        //     let result = query_attachment(&app.pool, &query, &page).await?;
+        //     Ok(HttpResponse::Ok().json(ApiResponse::normal(result)))
+        // }
         _ => Ok(HttpResponse::Ok().json(ApiResponse::empty())),
     };
 }
