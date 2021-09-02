f3974b8fe4e529c723bf74afd7cd152662e0d152
B635 <xck15221368562@gmail.com>
2021-09-02 21:36:08 +0800

    修改查询课表模块,学年形式改变

--- src/services/handlers/edu.rs
+++ src/services/handlers/edu.rs
@@ -1,9 +1,5 @@
 //! This module includes interfaces about course, major and score.
 
-use actix_web::{get, web, HttpResponse};
-use serde::{Deserialize, Serialize};
-use serde_json::json;
-
 use crate::bridge::{
     HostError, MajorRequest, RequestFrame, RequestPayload, ResponsePayload, SchoolYear, ScoreRequest,
     Semester, TimeTableRequest,
@@ -14,9 +10,17 @@ use crate::models::user::Person;
 use crate::models::{CommonError, PageView};
 use crate::services::response::ApiResponse;
 use crate::services::{AppState, JwtToken};
+use actix_web::{get, web, HttpResponse};
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 const CAMPUS_XUHUI: i32 = 2;
 const CAMPUS_FENGXIAN: i32 = 1;
+
+lazy_static! {
+    static ref YEAR_REGEX: Regex = Regex::new(r"\d+").unwrap();
+}
 
 #[derive(serde::Deserialize, sqlx::FromRow)]
 pub struct ClassroomQuery {
@@ -63,7 +67,7 @@ pub async fn query_available_classrooms(
 
 #[derive(Debug, Deserialize)]
 pub struct TimeTableQuery {
-    pub year: i32,
+    pub year: String,
     pub semester: i32,
 }
 
@@ -74,8 +78,15 @@ pub async fn query_timetable(
     params: web::Query<TimeTableQuery>,
 ) -> Result<HttpResponse> {
     let params = params.into_inner();
-
-    let year = SchoolYear::SomeYear(params.year);
+    let first_year = YEAR_REGEX
+        .captures(&params.year)
+        .unwrap()
+        .get(0)
+        .unwrap()
+        .as_str()
+        .parse()
+        .unwrap();
+    let year = SchoolYear::SomeYear(first_year);
     let semester = match params.semester {
         1 => Semester::FirstTerm,
         2 => Semester::SecondTerm,
