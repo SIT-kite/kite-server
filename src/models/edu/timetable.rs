17950e7499515bd3887320163580437d684c999b
zzq <2428698039@qq.com>
2021-09-30 00:39:33 +0800

    更新 timetable 模块, 优化常量, 补充大量注释, 新增部分测试代码，修复了导出日程uid不唯一的bug

--- src/models/edu/timetable.rs
+++ src/models/edu/timetable.rs
@@ -5,6 +5,7 @@ use chrono::{Date, DateTime, Duration, FixedOffset, NaiveDateTime, NaiveTime, Ti
 use ics::Event as CalEvent;
 
 use crate::bridge::Course;
+use crate::models::edu::{CAMPUS_FENGXIAN, CAMPUS_XUHUI};
 
 type ScheduleType = HashMap<&'static str, HashMap<&'static str, Vec<[&'static str; 2]>>>;
 
@@ -98,10 +99,11 @@ fn get_semester_start_date() -> Date<FixedOffset> {
     FixedOffset::east(8 * 3600).ymd(2021, 9, 6)
 }
 
+/// 获取该学期的某周的第几天的时间
 fn get_semester_day_offset<Tz: TimeZone>(
-    start_date: &Date<Tz>,
-    week_index: i32,
-    day_in_week: i32,
+    start_date: &Date<Tz>, // 学期开始时间
+    week_index: i32,       // 第几周，索引从1开始
+    day_in_week: i32,      // 该周的第几天，周天为7
 ) -> Date<Tz> {
     // Assume that, the offset of the first day of the semester is 1.
     let day_offset = (week_index - 1) * 7 + day_in_week - 1;
@@ -112,6 +114,7 @@ fn get_semester_day_offset<Tz: TimeZone>(
 #[test]
 fn test_get_semester_day_offset() {
     let offset = FixedOffset::east(8 * 3600);
+    // 学期开始时间
     let start_date = offset.ymd(2021, 9, 6);
 
     // 第一周 周一 -> 9 月 6 日
@@ -123,7 +126,10 @@ fn test_get_semester_day_offset() {
     assert_eq!(day7, offset.ymd(2021, 9, 13));
 }
 
-/// Convert "12:05" to 43260
+/// 获得从00:00开始到传入时间所经过的秒数
+/// 注意： 该函数不会检查字符串格式是否正确
+/// eg: Convert "12:05" to 43500
+/// result: 12 * 3600 + 5 * 60 = 43500
 fn unchecked_time_string_to_secs_offset(time_string: &str) -> i64 {
     let (a, b) = time_string.split_once(":").unwrap();
     let (x, y) = (
@@ -134,18 +140,26 @@ fn unchecked_time_string_to_secs_offset(time_string: &str) -> i64 {
     x * 3600 + y * 60
 }
 
+#[test]
+fn test_unchecked_time_string_to_secs_offset() {
+    assert_eq!(12 * 3600 + 5 * 60, unchecked_time_string_to_secs_offset("12:05"));
+}
+
+///
 fn get_course_start_end_time(
-    campus: i32,
-    building: &str,
-    index_start: usize,
-    index_end: usize,
+    campus: i32,        // 校区
+    building: &str,     // 在哪上课
+    index_start: usize, // 第几节上课
+    index_end: usize,   // 第几节下课
 ) -> (NaiveTime, NaiveTime) {
     let campus = match campus {
-        1 => "奉贤校区",
-        2 => "徐汇校区",
+        CAMPUS_FENGXIAN => "奉贤校区",
+        CAMPUS_XUHUI => "徐汇校区",
         _ => unreachable!(),
     };
     let result = &SCHEDULE[campus];
+
+    // 该地点的时刻表
     let building_table = result.get(building).unwrap_or_else(|| &result["default"]);
 
     let time_array = [
@@ -155,6 +169,8 @@ fn get_course_start_end_time(
     (time_array[0], time_array[1])
 }
 
+/// 一天上课节数使用位来控制，第n节课占用第n位,
+/// 注意： 第0位空出，始终为0
 fn split_start_end_index(mut time_index: i32) -> (i32, i32) {
     // When time_index = (110) in binary, it represents 1-2 in day.
     let (mut index_start, mut index_end);
@@ -174,7 +190,6 @@ fn split_start_end_index(mut time_index: i32) -> (i32, i32) {
     (index_start, index_end)
 }
 
-// Campus: 1-CAMPUS_FENGXIAN, 2-CAMPUS_XUHUI
 fn get_course_start_end_date_time<Tz: TimeZone>(
     day: Date<Tz>,
     campus: i32,
@@ -207,14 +222,16 @@ fn test_get_course_start_end_time() {
 #[test]
 fn test_get_course_start_end_date_time() {}
 
+/// 校区名转数字
 fn campus_to_i32(campus: &str) -> i32 {
     match campus {
-        "奉贤校区" => 1,
-        "徐汇校区" => 2,
+        "奉贤校区" => CAMPUS_FENGXIAN,
+        "徐汇校区" => CAMPUS_XUHUI,
         _ => unreachable!(),
     }
 }
 
+/// 提取上课地点中的教学楼名称
 fn get_building_by_place(place: &str) -> String {
     if place.starts_with("一教") {
         "一教".to_string()
@@ -225,16 +242,18 @@ fn get_building_by_place(place: &str) -> String {
     }
 }
 
+/// 将带时区的时间转化为ICS时间字符串
 fn to_datetime_string<Tz: chrono::TimeZone>(date_time: &DateTime<Tz>) -> String {
     date_time.naive_utc().format("%Y%m%dT%H%M%SZ").to_string()
 }
 
+/// 向日程表中添加一门课
 fn add_course_to_calendar<'a>(calendar: &mut ics::ICalendar<'a>, course: &'a Course, alarm_offset: i32) {
     use ics::{properties::*, Alarm};
 
     let semester_date = get_semester_start_date();
     let campus = campus_to_i32(&course.campus);
-    let building = if campus == 1 {
+    let building = if campus == CAMPUS_FENGXIAN {
         get_building_by_place(&course.place)
     } else {
         "default".to_string()
@@ -246,9 +265,6 @@ fn add_course_to_calendar<'a>(calendar: &mut ics::ICalendar<'a>, course: &'a Cou
         .into_iter()
         .filter(|week| course.week & (1 << week) != 0)
         .for_each(|week| {
-            let uuid = format!("SIT-KITE-{}", uuid::Uuid::new_v4());
-            let mut event = CalEvent::new(uuid, ts.clone());
-
             let course_date = get_semester_day_offset(&semester_date, week, course.day);
             let (index_start, index_end) = split_start_end_index(course.time_index);
             let (course_start, course_end) =
@@ -270,7 +286,20 @@ fn add_course_to_calendar<'a>(calendar: &mut ics::ICalendar<'a>, course: &'a Cou
                     course.teacher.join(", ")
                 )
             };
-            event.push(DtStart::new(to_datetime_string(&course_start)));
+
+            // 使用开始时间，地点名，课程名做md5
+            let course_start_time_str = to_datetime_string(&course_start);
+
+            let course_md5 = md5::compute(format!(
+                "{}{}{}",
+                course_start_time_str, course.place, course.course_name
+            ));
+            let course_md5 = format!("{:x}", course_md5);
+            let event_uid = format!("SIT-KITE-{}", course_md5);
+
+            let mut event = CalEvent::new(event_uid, ts.clone());
+
+            event.push(DtStart::new(course_start_time_str));
             event.push(DtEnd::new(to_datetime_string(&course_end)));
             event.push(Location::new(&course.place));
             event.push(Summary::new(&course.course_name));
@@ -287,19 +316,41 @@ fn add_course_to_calendar<'a>(calendar: &mut ics::ICalendar<'a>, course: &'a Cou
         });
 }
 
+/// 导出课表到日历的日程
 pub fn export_course_list_to_calendar(course_list: &[Course], alarm: i32) -> Vec<u8> {
     let mut calendar = ics::ICalendar::new(
         "2.0",
         "-//Yiban Station of Shanghai Institute of Technology//SIT-KITE//EN",
     );
-    let mut result = Vec::<u8>::new();
 
-    for course in course_list.iter() {
+    for course in course_list {
         add_course_to_calendar(&mut calendar, course, alarm);
     }
-    calendar.write(&mut result);
+
+    let mut result = Vec::<u8>::new();
+    calendar.write(&mut result).unwrap();
 
     result
+}
+
+#[test]
+fn test_export_course_list_to_calendar() {
+    let course = Course {
+        course_name: "Test".to_string(),
+        day: 1,
+        time_index: 0b1110,
+        week: 0b111,
+        place: "一教A101".to_string(),
+        teacher: vec!["Hello".to_string(), "World".to_string()],
+        campus: "奉贤校区".to_string(),
+        credit: 0.0,
+        hours: 2,
+        dyn_class_id: "".to_string(),
+        course_id: "".to_string(),
+    };
+    let export = export_course_list_to_calendar(&[course], 15);
+    let s = String::from_utf8(export).unwrap();
+    println!("{}", s);
 }
 
 pub fn generate_sign(uid: i32) -> String {
