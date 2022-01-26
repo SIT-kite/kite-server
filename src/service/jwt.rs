db47dda0f8cd2b0b59dafc2d9fe00ae792c9a49c
sunnysab <i@sunnysab.cn>
2022-01-26 21:17:14 +0800

    Add: 为 JWT 处理添加单元测试, 修复 decode 时的问题.

--- src/service/jwt.rs
+++ src/service/jwt.rs
@@ -25,7 +25,10 @@ impl JwtToken {
     pub fn decode(token: &str) -> Option<Self> {
         let key = CONFIG.get().unwrap().secret.as_str();
         let decoding_key = jsonwebtoken::DecodingKey::from_secret(key.as_ref());
-        let option = jsonwebtoken::Validation::default();
+        let option = jsonwebtoken::Validation {
+            validate_exp: false,
+            ..jsonwebtoken::Validation::default()
+        };
         let token_data = jsonwebtoken::decode::<Self>(token, &decoding_key, &option);
 
         if let Ok(token_data) = token_data {
@@ -51,5 +54,42 @@ impl<'a> FromRequest<'a> for JwtToken {
             .and_then(JwtToken::decode)
             .ok_or_else(|| ApiError::custom(100, "凭据无效"))?;
         Ok(token)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::config::{load_config, CONFIG};
+    use crate::service::jwt::JwtToken;
+
+    #[test]
+    fn test_jwt_decoder() {
+        CONFIG.set(load_config());
+
+        let token_string = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjMsInJvbGUiOjEwfQ.FHylo2zVAvIkmOcyHoIX4PnJw3HF2EwZWx5dqTVHxhc";
+        let token = JwtToken::decode(token_string);
+
+        if let Some(token) = token {
+            assert_eq!(token.uid, 3);
+            assert_eq!(token.role, 10);
+        } else {
+            panic!("token decode failed")
+        }
+    }
+
+    #[test]
+    fn test_jwt_token() {
+        CONFIG.set(load_config());
+
+        let token = JwtToken::new(100, 50);
+        let token_string = token.encode();
+        let token2 = JwtToken::decode(&token_string);
+
+        if let Some(token2) = token2 {
+            assert_eq!(token.uid, token2.uid);
+            assert_eq!(token.role, token2.role);
+        } else {
+            panic!("token decode failed");
+        }
     }
 }
