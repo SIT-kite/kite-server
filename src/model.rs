5fc9c30ebfd8a6895684f36e1a44b2b1f5dffea2
sunnysab <i@sunnysab.cn>
2022-01-28 00:45:48 +0800

    Add: 添加分页 model.

--- src/model.rs
+++ src/model.rs
@@ -6,5 +6,46 @@ pub mod notice;
 pub mod user;
 pub mod weather;
 
-pub(crate) const CAMPUS_FENGXIAN: i32 = 1;
-pub(crate) const CAMPUS_XUHUI: i32 = 2;
+pub const CAMPUS_FENGXIAN: i32 = 1;
+pub const CAMPUS_XUHUI: i32 = 2;
+
+const DEFAULT_PAGE_INDEX: u16 = 0;
+const DEFAULT_ITEM_COUNT: u16 = 20;
+
+/// Page parameters for list pagination
+#[derive(Serialize, Deserialize, Default)]
+pub struct PageView {
+    /// Page index, 1 is the minimum value
+    pub index: Option<u16>,
+    /// Page count, 1 is the minimum value
+    pub count: Option<u16>,
+}
+
+impl PageView {
+    /// Create a new page view structure
+    pub fn new() -> Self {
+        PageView::default()
+    }
+    /// Get validated index
+    pub fn index(&self) -> u16 {
+        if let Some(index) = self.index {
+            if index > 0 {
+                return index;
+            }
+        }
+        DEFAULT_PAGE_INDEX
+    }
+    /// Get validated item count value
+    pub fn count(&self, max_count: u16) -> u16 {
+        if let Some(count) = self.count {
+            if count < max_count {
+                return count;
+            }
+        }
+        DEFAULT_ITEM_COUNT
+    }
+    /// Calculate offset
+    pub fn offset(&self, max_count: u16) -> u16 {
+        self.count(max_count) * (self.index() - 1)
+    }
+}
