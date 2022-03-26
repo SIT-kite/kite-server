4dc23ce5769bd78ddd5702e2567b52d7d9c085b8
sunnysab <i@sunnysab.cn>
2022-03-26 21:34:34 +0800

    Refactor: "图书馆" 模块目录结构.

---
+++ src/model/library/appointment.rs
@@ -0,0 +1,148 @@
+use super::LibraryError;
+use super::{Application, Notice, Status};
+use crate::error::{ApiError, Result};
+use chrono::{DateTime, Local};
+use num_derive::ToPrimitive;
+use rsa::{pkcs8::FromPrivateKey, PaddingScheme, PublicKey, RsaPrivateKey};
+use sqlx::PgPool;
+
+/// 获取图书馆公告
+pub async fn get_notice(pool: &PgPool) -> Result<Option<Notice>> {
+    let notice: Option<Notice> = sqlx::query_as("SELECT html, ts FROM library.notice ORDER BY ts DESC LIMIT 1;")
+        .fetch_optional(pool)
+        .await?;
+    Ok(notice)
+}
+
+/// 获取预约状态
+pub async fn get_status(pool: &PgPool, date: i32) -> Result<Vec<Status>> {
+    let result = sqlx::query_as(
+        "SELECT generate_series AS period, COALESCE(applied, 0) AS applied 
+        FROM generate_series($1, $2)
+        LEFT JOIN (
+            SELECT period, CAST(COUNT(*) AS int) AS applied
+            FROM library.application
+            GROUP BY period
+            ORDER BY period
+        ) AS applied_count
+        ON period = generate_series;",
+    )
+    .bind(date * 10 + 1)
+    .bind(date * 10 + 3)
+    .fetch_all(pool)
+    .await?;
+    Ok(result)
+}
+
+/// 获取预约列表
+///
+/// 请不要同时提供或不提供 period/user 参数.
+pub async fn get_applications(
+    pool: &PgPool,
+    period: Option<i32>,
+    user: Option<String>,
+    date: Option<i32>,
+) -> Result<Vec<Application>> {
+    if date.is_none() && ((period.is_none() && user.is_none()) || (period.is_some() && user.is_some())) {
+        return Ok(vec![]);
+    }
+    let applications = sqlx::query_as(
+        "SELECT id, period, \"user\", index, status, ts
+        FROM library.application_view
+        WHERE (period = $1 OR $1 IS NULL)
+            AND (\"user\" = $2 OR $2 IS NULL)\
+            AND (period / 10 = $3 OR $3 IS NULL);",
+    )
+    .bind(period)
+    .bind(user)
+    .bind(date)
+    .fetch_all(pool)
+    .await?;
+    Ok(applications)
+}
+
+/// 查询单条申请记录
+pub async fn query_application_by_id(pool: &PgPool, id: i32) -> Result<Option<Application>> {
+    let result = sqlx::query_as(
+        "SELECT id, period, \"user\", index, status, ts
+        FROM library.application_view
+        WHERE id = $1
+        LIMIT 1;",
+    )
+    .bind(id)
+    .fetch_optional(pool)
+    .await?;
+    Ok(result)
+}
+
+/// 生成加密的二维码信息
+pub async fn get_code(pool: &PgPool, id: i32, private_key: &str) -> Result<String> {
+    let application = query_application_by_id(pool, id)
+        .await?
+        .ok_or(ApiError::new(LibraryError::NoSuchItem))?;
+
+    #[derive(serde::Serialize)]
+    pub struct ApplicationCode {
+        pub application: Application,
+        pub generation_ts: DateTime<Local>,
+    }
+
+    let code_structure = ApplicationCode {
+        application,
+        generation_ts: Local::now(),
+    };
+    let content = serde_json::to_string(&code_structure).unwrap();
+
+    let mut rng = rand::thread_rng();
+    let key = RsaPrivateKey::from_pkcs8_pem(private_key).expect("Invalid Rsa key.");
+    let result = key
+        .encrypt(&mut rng, PaddingScheme::PKCS1v15Encrypt, content.as_bytes())
+        .unwrap();
+
+    Ok(base64::encode(result))
+}
+
+#[derive(sqlx::FromRow)]
+pub struct ApplyResult {
+    pub id: Option<i32>,
+    pub index: i32,
+}
+
+/// 预约座位
+pub async fn apply(pool: &PgPool, uid: i32, period: i32) -> Result<ApplyResult> {
+    // library.apply(_uid int, _period int, max_seat int)
+    let result: ApplyResult = sqlx::query_as("SELECT id, index FROM library.apply($1, $2, 275);")
+        .bind(uid)
+        .bind(period)
+        .fetch_one(pool)
+        .await?;
+    if result.id.is_none() {
+        return Err(ApiError::new(LibraryError::AlreadyFull));
+    }
+    Ok(result)
+}
+
+/// 更新预约状态
+pub async fn update_application(pool: &PgPool, apply_id: i32, status: i32) -> Result<()> {
+    sqlx::query("UPDATE library.application SET status = $1 WHERE id = $2;")
+        .bind(status)
+        .bind(apply_id)
+        .execute(pool)
+        .await?;
+    Ok(())
+}
+
+/// 取消预约
+pub async fn cancel(pool: &PgPool, apply_id: i32) -> Result<()> {
+    let record = query_application_by_id(pool, apply_id)
+        .await?
+        .ok_or(ApiError::new(LibraryError::NoSuchItem))?;
+    if record.status == 1 {
+        return Err(ApiError::new(LibraryError::CanNotCancel));
+    }
+    sqlx::query("DELETE FROM library.application WHERE id = $1;")
+        .bind(apply_id)
+        .execute(pool)
+        .await?;
+    Ok(())
+}
---
+++ src/model/library/period.rs
@@ -0,0 +1,44 @@
+use chrono::{Date, DateTime, Datelike, Local, Timelike};
+use serde_json::json;
+
+/// 图书馆开馆区间
+static OPENING_PERIOD: &[(i32, i32); 3] = &[(830, 1130), (1300, 1600), (1730, 2100)];
+
+/// 根据时间推算当日场次
+fn get_period_index(datetime: DateTime<Local>) -> Option<i32> {
+    let time = datetime.hour() * 100 + datetime.minute();
+
+    for i in 0..OPENING_PERIOD.len() {
+        let (start, end) = (OPENING_PERIOD[i].0 as u32, OPENING_PERIOD[i].1 as u32);
+
+        if time >= start as u32 && time <= end {
+            return Some((i + 1) as i32);
+        }
+    }
+    None
+}
+
+pub fn make_period_by_datetime(datetime: DateTime<Local>) -> Option<i32> {
+    // 周一、二不开放
+    if datetime.weekday().num_days_from_monday() == 1 || datetime.weekday().num_days_from_monday() == 2 {
+        return None;
+    }
+
+    get_period_index(datetime).map(|index| {
+        datetime.year() % 100 * 100000 + datetime.month() as i32 * 1000 + datetime.day() as i32 * 10 + index
+    })
+}
+
+pub fn get_current_period() -> Option<i32> {
+    make_period_by_datetime(Local::now())
+}
+
+/// 获取第 i 场开放的开始时间和结束时间
+pub fn get_period_range(base_date: Date<Local>, index: i32) -> (DateTime<Local>, DateTime<Local>) {
+    let period_pair: (i32, i32) = OPENING_PERIOD[index as usize - 1];
+    let (start, end) = (period_pair.0, period_pair.1);
+
+    let start_ts = base_date.and_hms((start / 100) as u32, (start % 100) as u32, 0);
+    let end_ts = base_date.and_hms((end / 100) as u32, (end % 100) as u32, 0);
+    (start_ts, end_ts)
+}
