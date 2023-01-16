ca3660adbc7baa1880f1ed33063a17de9b90d90c
sunnysab <i@sunnysab.cn>
2023-01-16 20:55:09 +0800

    Optimize code structure, add convert module in model.rs

--- src/model.rs
+++ src/model.rs
@@ -1,33 +1,36 @@
-use chrono::{DateTime, Local, NaiveDateTime, TimeZone, Timelike};
-use prost_types::Timestamp;
+pub use convert::{ToDateTime, ToTimestamp};
 
 pub mod badge;
 pub mod balance;
 
-// TODO: Add mod here.
-pub trait ToDateTime {
-    fn timestamp(self) -> DateTime<Local>;
-}
+pub mod convert {
+    use chrono::{DateTime, Local, NaiveDateTime, TimeZone, Timelike};
+    use prost_types::Timestamp;
 
-pub trait ToTimestamp {
-    fn datetime(self) -> Timestamp;
-}
-
-impl ToDateTime for Timestamp {
-    fn timestamp(self) -> DateTime<Local> {
-        let (secs, nsecs) = (self.seconds, self.nanos);
-        let dt = NaiveDateTime::from_timestamp_opt(secs, nsecs as u32).unwrap();
-
-        Local::from_local_datetime(&Local, &dt).unwrap()
+    pub trait ToDateTime {
+        fn timestamp(self) -> DateTime<Local>;
     }
-}
 
-impl ToTimestamp for DateTime<Local> {
-    fn datetime(self) -> Timestamp {
-        let (secs, nsecs) = (self.timestamp(), self.nanosecond());
-        Timestamp {
-            seconds: secs,
-            nanos: nsecs as i32,
+    pub trait ToTimestamp {
+        fn datetime(self) -> Timestamp;
+    }
+
+    impl ToDateTime for Timestamp {
+        fn timestamp(self) -> DateTime<Local> {
+            let (secs, nsecs) = (self.seconds, self.nanos);
+            let dt = NaiveDateTime::from_timestamp_opt(secs, nsecs as u32).unwrap();
+
+            Local::from_local_datetime(&Local, &dt).unwrap()
+        }
+    }
+
+    impl ToTimestamp for DateTime<Local> {
+        fn datetime(self) -> Timestamp {
+            let (secs, nsecs) = (self.timestamp(), self.nanosecond());
+            Timestamp {
+                seconds: secs,
+                nanos: nsecs as i32,
+            }
         }
     }
 }
