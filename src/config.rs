42d5e8a9ce4682db11884767b233c56bd0ef0c7b
sunnysab <i@sunnysab.cn>
2023-01-16 16:06:36 +0800

    Add config.rs

---
+++ src/config.rs
@@ -0,0 +1,38 @@
+use once_cell::sync::OnceCell;
+use serde::Deserialize;
+
+pub static CONFIG: OnceCell<ServerConfig> = OnceCell::new();
+
+// Look and rename kite.example.toml
+const DEFAULT_CONFIG_PATH: &str = "kite.toml";
+
+#[derive(Deserialize)]
+pub struct ServerConfig {
+    /// Bind address with type "x.x.x.x:port"
+    /// Usually "0.0.0.0:443"
+    pub bind: String,
+    /// JWT Secret for encrypt.
+    pub secret: String,
+    /// Database for postgresql.
+    pub db: String,
+
+    /* External API */
+    /// QWeather.com API key.
+    pub qweather_key: String,
+}
+
+fn get_config_path() -> String {
+    std::env::var_os("KITE_CONFIG")
+        .and_then(|s| Some(s.into_string().unwrap()))
+        .unwrap_or(DEFAULT_CONFIG_PATH.to_string())
+}
+
+/// Load the global configuration on startup.
+pub fn load_config() -> ServerConfig {
+    let path = get_config_path();
+
+    std::fs::read_to_string(&path)
+        .map_err(anyhow::Error::new)
+        .and_then(|f| toml::from_str(&f).map_err(anyhow::Error::new))
+        .unwrap_or_else(|e| panic!("Failed to parse {:?}: {}", path, e))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use crate::config::load_config;
+
 mod service;
+mod config;
 
 #[tokio::main]
 async fn main() {
+    config::CONFIG
+        .set(config::load_config())
+        .expect("Failed to load configuration file, which is kite.toml by default and can be set by KITE_CONFIG.");
 
     tokio::join!(
         // Run grpc server
