c67bfaaa2f9e8c44ec6a6b5cb6ccbde01187f4e4
sunnysab <i@sunnysab.cn>
2023-01-16 11:54:43 +0800

    Add tonic files

---
+++ build.rs
@@ -0,0 +1,28 @@
+
+fn main() {
+    let out_dir = std::path::PathBuf::from("src/protocol");
+
+    tonic_build::configure()
+        .build_server(true)
+        .out_dir(out_dir)
+        .compile(&[
+            // Basic
+                "proto/template.proto",
+                "proto/token.proto",
+                "proto/typing.proto",
+            // Test
+                "proto/ping.proto",
+            // Service
+                "proto/badge.proto",
+                "proto/balance.proto",
+                "proto/board.proto",
+                "proto/classroom_browser.proto",
+                "proto/exception.proto",
+                "proto/freshman.proto",
+                "proto/game.proto",
+                "proto/user.proto",
+                "proto/yellow_page.proto",
+        ],
+                 &["proto"])
+        .unwrap();
+}
