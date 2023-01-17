b1ba9973c90d899ad9364204a5149f8b6ef90879
sunnysab <i@sunnysab.cn>
2023-01-17 22:03:21 +0800

    Fix broken proto path

--- service-v3/build.rs
+++ service-v3/build.rs
@@ -22,28 +22,28 @@ fn main() {
     tonic_build::configure()
         .build_server(true)
         .build_client(false)
-        .file_descriptor_set_path("target/compiled-descriptor.bin")
+        .file_descriptor_set_path("../target/compiled-descriptor.bin")
         .out_dir(out_dir)
         .compile(
             &[
                 // Basic
-                "proto/template.proto",
-                "proto/token.proto",
-                "proto/typing.proto",
+                "../proto/template.proto",
+                "../proto/token.proto",
+                "../proto/typing.proto",
                 // Test
-                "proto/ping.proto",
+                "../proto/ping.proto",
                 // Service
-                "proto/badge.proto",
-                "proto/balance.proto",
-                "proto/board.proto",
-                "proto/classroom_browser.proto",
-                "proto/exception.proto",
-                "proto/freshman.proto",
-                "proto/game.proto",
-                "proto/user.proto",
-                "proto/yellow_page.proto",
+                "../proto/badge.proto",
+                "../proto/balance.proto",
+                "../proto/board.proto",
+                "../proto/classroom_browser.proto",
+                "../proto/exception.proto",
+                "../proto/freshman.proto",
+                "../proto/game.proto",
+                "../proto/user.proto",
+                "../proto/yellow_page.proto",
             ],
-            &["proto"],
+            &["../proto"],
         )
         .unwrap();
 }
