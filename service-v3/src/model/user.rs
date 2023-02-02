a733cfe00806ea06ad55705d7ea1b5ba02c37084
sunnysab <i@sunnysab.cn>
2023-02-02 20:32:40 +0800

    Add user model.

--- service-v3/src/model/user.rs
+++ service-v3/src/model/user.rs
@@ -16,5 +16,31 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-mod connector;
-mod stream;
+use chrono::{DateTime, Local};
+use regex_macro::regex;
+
+#[derive(Debug, sqlx::FromRow)]
+pub struct User {
+    /// 用户 ID
+    pub uid: i32,
+    /// 用户学号
+    pub account: String,
+    /// 用户创建日期
+    pub create_time: DateTime<Local>,
+    /// 用户角色
+    pub role: i32,
+    /// 账户是否被禁用
+    pub is_block: bool,
+}
+
+pub struct Validator;
+
+impl Validator {
+    pub fn validate_username(account: &str) -> bool {
+        if account.len() != 4 && account.len() != 10 && account.len() != 9 {
+            return false;
+        }
+        let regex = regex!(r"^((\d{2}6\d{6})|(\d{4})|(\d{6}[YGHE\d]\d{3}))$");
+        return regex.is_match(&account.to_uppercase());
+    }
+}
