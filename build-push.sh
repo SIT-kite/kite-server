56a6728e487bbb2e1eb0037b5f8ba5859c0e1040
sunnysab <i@sunnysab.cn>
2023-02-09 22:55:17 +0800

    Add deploy script (for testing).

---
+++ build-push.sh
@@ -0,0 +1,24 @@
+#
+# 上应小风筝  便利校园，一步到位
+# Copyright (C) 2020-2023 上海应用技术大学 上应小风筝团队
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+cargo build --release
+cp target/release/loader kite-server-v3
+strip -s kite-server-v3
+
+scp kite-server-v3 kite@origin.kite.sunnysab.cn:~/kite-server-v3
+ssh kite@origin.kite.sunnysab.cn "systemctl --user restart kite3.service"
